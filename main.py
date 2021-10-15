54e1e27cb43c57f7399e7966d2aeb26ce8eed686
= <=>
2021-10-15 15:56:06 +0200

    add and run routes

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add():
+    num1 = int(request.args.get('num1'))
+    num2 = int(request.args.get('num2'))
+    aws = num1 + num2
+
+    return jsonify(aws)
+
+@app.route('/run')
+def run():
+    try:
+        script = request.args.get('script')
+    except Exception:
+        return jsonify('No script given')
+    
+    try:
+        os.system(f"python3 scripts/{script}")
+    except Exception:
+        return jsonify('No script found')
+
+    return jsonify('response: {200}')
+
+app.run()
